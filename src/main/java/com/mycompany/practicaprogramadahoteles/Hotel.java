ec04060905d602c0ff58e5ce20e807ab8144938e
Gabriel Castro <gcastroa16@gmail.com>
2024-02-26 21:59:02 -0600

    hotfix (#3)
    * menu y matrices de hotel
    
    * submenu1
    
    * Update Hotel.java
    
    * Update Hotel.java

--- src/main/java/com/mycompany/practicaprogramadahoteles/Hotel.java
+++ src/main/java/com/mycompany/practicaprogramadahoteles/Hotel.java
@@ -34,7 +34,6 @@ public class Hotel {
         }
     }
 
-
     public static void submenu1(Hotel[] hoteles) {
         String menu;
         int opcion;
@@ -85,7 +84,7 @@ public class Hotel {
     public static void listarHabitacionesDisponibles(Hotel[] hoteles, int indiceHotel) {
         Hotel hotel = hoteles[indiceHotel]; // Obtiene el hotel específico según el índice proporcionado
         // Imprime el nombre del hotel
-        System.out.println("-----------------------------Listado de Habitaciones de Hotel"+hotel.nombre+"-----------------------------\n");
+        System.out.println("Hotel: " + hotel.nombre);
         // Este bucle recorre cada torre en el hotel actual
         for (int i = 0; i < hotel.torres.length; i++) {
             // Imprime el número de torre
@@ -101,39 +100,15 @@ public class Hotel {
                         System.out.print("[ ]"); // Habitación disponible
                     } else {
                         System.out.print("[X]"); // Habitación ocupada
-
-    public static void listarHabitacionesDisponibles(Hotel[] hoteles) {
-        // Utiliza un for each para recorrer el array de hoteles
-        for (Hotel hotel : hoteles) {
-            // Imprime el nombre del hotel
-            System.out.println("Hotel: " + hotel.nombre);
-            // Este bucle recorre cada torre en el hotel actual
-            for (int i = 0; i < hotel.torres.length; i++) {
-                // Imprime el número de torre
-                System.out.println("Torre " + (i + 1) + ":");
-                // Este bucle recorre cada piso de la torre
-                for (int j = 0; j < hotel.torres[i].pisos.length; j++) {
-                    // Este bucle recorre cada habitación del piso
-                    for (int k = 0; k < hotel.torres[i].pisos[j].habitaciones.length; k++) {
-                        // Obtiene la referencia de la habitación actual
-                        Habitacion habitacion = hotel.torres[i].pisos[j].habitaciones[k];
-                        // Imprime la representación visual de la habitación
-                        if (habitacion.disponible) {
-                            System.out.print("[ ]"); // Habitación disponible
-                        } else {
-                            System.out.print("[X]"); // Habitación ocupada
-                        }
-
                     }
-                    // Nueva línea después de imprimir todas las habitaciones de un piso
-                    System.out.println();
                 }
+                // Nueva línea después de imprimir todas las habitaciones de un piso
+                System.out.println();
             }
             // Nueva línea después de imprimir todas las habitaciones de una torre
             System.out.println();
         }
     }
-
 
     public static void reservarAutomaticamente(Hotel[] hoteles, Persona persona) {
         //Este bucle itera sobre cada hotel
