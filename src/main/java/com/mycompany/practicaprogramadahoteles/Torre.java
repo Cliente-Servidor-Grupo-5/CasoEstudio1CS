58e25029fdd765c51d128ce09b01b26713fd5a69
Gabriel Castro <gcastroa16@gmail.com>
2024-03-01 21:40:40 -0600

    Update Torre.java

--- src/main/java/com/mycompany/practicaprogramadahoteles/Torre.java
+++ src/main/java/com/mycompany/practicaprogramadahoteles/Torre.java
@@ -1,25 +1,28 @@
-/***********************************************
-* Universidad Fidelitas                        *
-* Programacion Cliente Servidor Concurrente    *
-* @author isaac, javier, mason, gabriel        *
-* Fecha: 02/02/2024                            *
-* Nombre: ClaseSemana3                         *
-************************************************/
+/** *********************************************
+ * Universidad Fidelitas                        *
+ * Programacion Cliente Servidor Concurrente
+ *
+ *
+ * @author isaac, javier, mason, gabriel * Fecha: 02/02/2024 * Nombre:
+ * ClaseSemana3 *
+***********************************************
+ */
 package com.mycompany.practicaprogramadahoteles;
 
-
 public class Torre {
-    
     private int numero;
     //Se crea un atributo array de pisos
     Piso[] pisos;
     //Se crea un constructor y por medio de un ciclo for se crean las habitaciones
-    public Torre(int numPisos, int numHabitaciones) {
+    public Torre(int numPisos, int numHabitaciones, int numero) {
+        this.numero = numero;
         this.pisos = new Piso[numPisos];
         for (int i = 0; i < numPisos; i++) {
-            pisos[i] = new Piso(numHabitaciones);
+            pisos[i] = new Piso(numHabitaciones, i + 1);
         }
     }
 
-   
+    public int getNumero() {
+        return numero;
+    }
 }
